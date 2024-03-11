b6d31a1bc13b280dabb69315cecad012510fb05a
Matt_dev_on <127757483+Matt971x@users.noreply.github.com>
2024-03-11 18:02:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".icon-menu-close");
 const menu = document.querySelector(".menu");
 
-console.log(closeIcon);
 menuIcon.addEventListener("click", () => {
-  menu.style.width = "70%";
+  menu.style.display = "block";
   document.body.classList.toggle("dark-layout");
 });
 
 closeIcon.addEventListener("click", () => {
-  menu.style.width = "0%";
+  menu.style.display = "none";
   document.body.classList.remove("dark-layout");
 });
